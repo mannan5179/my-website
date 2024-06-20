748e84cff6d341b53270a3a3dc15dd07a2dfbcae
mannan5179 <mannan_201081@yahoo.com>
2024-06-20 21:44:22 +0600

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// scripts.js
+
+// Example: Toggle mobile menu
+const navToggle = document.querySelector('.nav-toggle');
+const navMenu = document.querySelector('.nav-menu');
+
+navToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('nav-menu--visible');
+});
